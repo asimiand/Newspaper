848646613c8db2873c7cd20de6969b76f71ab359
Adriana Simiand <asimiand@gmail.com>
2020-11-01 20:09:05 -0300

    html and css of subscribe

---
+++ main.js
@@ -0,0 +1,19 @@
+//VARIABLES
+
+var form = document.getElementsByClassName('form');
+var formLeft = document.getElementsByClassName('form-left');
+var formRight = document.getElementsByClassName('form-right');
+
+var name = document.getElementById('name');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+var password2 = document.getElementById('password2');
+var age = document.getElementById('age');
+var phone = document.getElementById('phone');
+var adress = document.getElementById('adress');
+var city = document.getElementById('city');
+var postalCode = document.getElementById('postalcode');
+var dni = document.getElementById('dni');
+
+
+
